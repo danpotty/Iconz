47578fc53cf375328b0d3ecec4fc6b4f0e16328a
danpotty <danpotmail@yahoo.com>
2016-02-16 19:38:24 -0800

    cleaning up models

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,6 @@ let mongoose = require('mongoose');
 let CommentSchema = new mongoose.Schema({
   message: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now },
-  likes: { type: Number, default: 0 },
   likers: [],
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   icon: { type: mongoose.Schema.Types.ObjectId, ref: 'Icon' }
